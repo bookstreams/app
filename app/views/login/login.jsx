a5523ca2c6d843091717bcb275b65e8048416d31
Paolo Scanferla <paolo.scanferla@mondora.com>
2014-12-05 02:19:47 +0100

    Added login

--- app/views/login/login.jsx
+++ app/views/login/login.jsx
@@ -1,10 +1,26 @@
-var React = require("react");
+var Bootstrap = require("react-bootstrap");
+var React     = require("react");
+
+var Button = Bootstrap.Button;
+var Col    = Bootstrap.Col;
 
 var Login = React.createClass({
+    login: function () {
+        Ceres.loginWithFacebook();
+    },
     render: function () {
         return (
             <div id="login">
-                login
+                <Col xs={12} className="text-center">
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <Button onClick={this.login}>
+                        Login
+                    </Button>
+                </Col>
             </div>
         );
     }
