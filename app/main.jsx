ef09fff8bc30fb2d54d557e28a93fd60302713f6
Paolo Scanferla <paolo.scanferla@mondora.com>
2014-12-06 19:20:28 +0100

    Set default routes

--- app/main.jsx
+++ app/main.jsx
@@ -12,17 +12,20 @@ var flux = new Fluxxor.Flux(stores, actions);
 require("./ceres.jsx").init(flux);
 
 // Views
-var Route = Router.Route;
-var Views = require("./views");
+var DefaultRoute = Router.DefaultRoute;
+var Route        = Router.Route;
+var Views        = require("./views");
 
 var routes = (
     <Route handler={Views.Root}>
-        <Route name="postLogin" handler={Views.PostLogin}>
+        <Route name="postLogin" path="/" handler={Views.PostLogin}>
             <Route name="add" handler={Views.Add} addHandlerKey={true} />
             <Route name="feed" handler={Views.Feed} addHandlerKey={true} />
             <Route name="geo" handler={Views.Geo} addHandlerKey={true} />
+            <DefaultRoute handler={Views.Feed} />
         </Route>
-        <Route name="login" handler={Views.Login} />
+        <Route name="login" path="/login" handler={Views.Login} />
+        <DefaultRoute handler={Views.Login} />
     </Route>
 );
 
